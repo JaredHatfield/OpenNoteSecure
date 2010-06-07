613ae90cc5c76f26362808d09c79d900bbd7cffc
Jared Hatfield <jjhatf02@gmail.com>
2010-06-07 10:07:04 -0400

    Initial Android project

---
+++ src/com/jaredhatfield/opennotesecure/OpenNoteSecure.java
@@ -0,0 +1,13 @@
+package com.jaredhatfield.opennotesecure;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OpenNoteSecure extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
