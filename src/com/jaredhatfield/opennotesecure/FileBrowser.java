30b55bcde9bd8f4305c9d95eb2be532a90bc9b3f
Jared Hatfield <jjhatf02@gmail.com>
2010-07-10 20:56:39 -0400

    Implemented the delete command which is accessable from the context menu.

--- src/com/jaredhatfield/opennotesecure/FileBrowser.java
+++ src/com/jaredhatfield/opennotesecure/FileBrowser.java
@@ -19,6 +19,7 @@ package com.jaredhatfield.opennotesecure;
 
 import java.io.File;
 import java.util.ArrayList;
+
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
@@ -30,11 +31,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 
 public class FileBrowser extends ListActivity 
@@ -81,10 +84,14 @@ public class FileBrowser extends ListActivity
 		this.newFileButton.setOnClickListener(this);
 		
 		// Add the ArrayAdapter to the ListView
-		this.setListAdapter(this.content);
+		//this.setListAdapter(this.content);
+		this.getListView().setAdapter(this.content);
 		
 		// Add the listener that detects when the button was pressed
 		this.getListView().setOnItemClickListener(this);
+		
+		// Register all list items for the context menu
+		registerForContextMenu(getListView());
 	}
 	
 	/**
@@ -158,11 +165,80 @@ public class FileBrowser extends ListActivity
 	}
 	
 	/**
-	 * 
+	 * Display the context menu for a specific item.
+	 */
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+	  super.onCreateContextMenu(menu, v, menuInfo);
+	  MenuInflater inflater = getMenuInflater();
+	  inflater.inflate(R.menu.edit_menu, menu);
+	  Log.i(OpenNoteSecure.TAG, "onCreateContextMenu");
+	}
+	
+	/**
+	 * Perform the action specified in the context menu.
+	 */
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		// Get the selected file
+		AdapterView.AdapterContextMenuInfo cmi = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo ();
+	    File f = (File)getListView ().getItemAtPosition (cmi.position);
+	    
+	    // Determine which button was clicked
+		switch (item.getItemId()) {
+			case R.id.menu_item_delete:
+				new DeleteFileTask().execute(f);
+				Log.i(OpenNoteSecure.TAG, "Delete button pressed");
+				return true;
+			default:
+				return super.onContextItemSelected(item);
+		}
+	}
+	
+	/**
+	 * Perform a file deletion.
+	 */
+	private class DeleteFileTask extends AsyncTask<File, Void, String>{
+
+		/**
+		 * Delete the file.
+		 */
+		@Override
+		protected String doInBackground(File... params) {
+			Log.i(OpenNoteSecure.TAG, "Delete doInBackground");
+			File file = params[0];
+			if(file != null && file.exists()){
+				Log.i(OpenNoteSecure.TAG, "Deleting " + file.getName());
+				file.delete();
+				return file.getName();
+			}
+			else{
+				return null;
+			}
+		}
+		
+		/**
+	      * Notify the user the file was deleted and update the ArrayAdapter.
+	      */
+	     protected void onPostExecute(String filename) {
+	    	 Log.i(OpenNoteSecure.TAG, "Delete onPostExecute");
+	    	 if(filename != null){
+	    		 Toast.makeText(getApplicationContext(), "Deleted " + filename, Toast.LENGTH_SHORT).show();
+	    		 FileManager.Instance().updateFileList(content);
+	    	 }
+	    	 else{
+	    		 Toast.makeText(getApplicationContext(), "Error: " + filename + " not deleted.", Toast.LENGTH_SHORT).show();
+	    	 }
+	     }
+
+	}
+	
+	/**
+	 * Creates a new file.
 	 */
 	private class CreateNewFile extends AsyncTask<String, Boolean, String> {
 		/**
-		 * 
+		 * Create the new file with the specified name.
 		 */
 	     protected String doInBackground(String... files) {
 	         String filename  = files[0];
@@ -177,7 +253,7 @@ public class FileBrowser extends ListActivity
 	     }
 	     
 	     /**
-	      * 
+	      * Notify the user the file was created and update the ArrayAdapter.
 	      */
 	     protected void onPostExecute(String filename) {
 	    	 newFileButton.requestFocus();
